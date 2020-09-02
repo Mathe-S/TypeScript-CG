9b28f6e5aa0638dcfea75ae1a7a7ff317a1c34b3
Mathe Sharvadze <sharvadzemat@gmail.com>
2020-09-02 11:14:27 +0400

    added maps app with google maps integration

---
+++ maps/src/index.ts
@@ -0,0 +1,10 @@
+import { User } from "./User";
+import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
+
+const customMap = new CustomMap("map");
+const user = new User();
+const company = new Company();
+
+customMap.createMarker(user);
+customMap.createMarker(company);
